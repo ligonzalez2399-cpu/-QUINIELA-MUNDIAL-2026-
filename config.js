c496118404a7a4cc1b581752f8c87c67442b7062
ligonzalez2399-cpu <li.gonzalez2399@gmail.com>
2026-06-11 10:29:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // ⚠️ Reemplaza esta URL con la tuya después de crear el Apps Script
 // Sigue las instrucciones en README.md para obtenerla
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwcCvPsDWWWk7SLWjvSpgT_w6xvwUgMRjdCYEGY_HhygEc6ikWyMzxLUrI9fb7qUhGs/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzkPymdZ1zRelbFDQercbY5OsTbSv-YO_OTEdtnCe1tJFm9Hp83POW0WAKW7fF5tM8f/exec';
